3a72f39c7dcae460c9de748589a0119936c8002b
Pierre/SurfaceBook <pierre.amelot@hotmail.fr>
2017-05-12 17:29:11 -0400

    Started the canals animation

--- SeeingJson/masterPieces.js
+++ SeeingJson/masterPieces.js
@@ -1,50 +1,93 @@
-queue()
-.defer(d3.xml, "path1.svg", "Data/amsterdamPaths")
-.await(ready);
+var mapUrl = "Data/amsterdamPaths/mapAndPaths.svg";
+var mapSvg = null;
+var numberPaths = 1;
+var resizingFactor = 1;
+var mapWidth = 1000.;
+var mapRatio = 1;
+var animationGroup = null;
 
-function ready(error, xml) {
+loadMap();
+animatePixels();
 
-  console.log(xml)
 
-  //Adding our svg file to HTML document
-  //var importedNode = document.importNode(xml.documentElement, true);
-  //d3.select("#amsterdamMap").node().appendChild(importedNode);
+function loadMap() {
+  //Load Map
+  xhr = new XMLHttpRequest();
+  xhr.open("GET",mapUrl,false);
+  xhr.overrideMimeType("image/svg+xml");
+  xhr.send("");
 
-var svg = d3.select("#amsterdamSVG");
+  //Embed Map in Webpage
+  mapSvg = d3.select(d3.select("#amsterdamMap")
+            .node()
+            .appendChild(xhr.responseXML.documentElement));
 
-  var path = svg.select("#LineToFollow"),
+  //Resize the map
+  resizingFactor = mapWidth / mapSvg.node().getBoundingClientRect().width;
+  mapRatio = mapSvg.node().getBoundingClientRect().width / mapSvg.node().getBoundingClientRect().height;
+  resizeMap(resizingFactor);
+
+  //Hide paths
+  for (var i = 0; i < numberPaths; i++){
+    d3.select("#path"+i)
+      .style("stroke-opacity", "0");
+  }
+}
+
+function resizeMap(scaleFactor){
+  mapSvg.attr("width", mapWidth)
+        .attr("height", mapWidth / mapRatio);
+}
+
+function animatePixels(){
+  //Create Animation Group in the Svg
+  animationGroup = mapSvg.append("g")
+                          .attr("id", "animationGroup");
+
+  //Animate the circles
+  for (var i = 0; i < numberPaths; i++){
+    animateCircle("#path"+i);
+  }
+}
+
+function animateCircle(pathId){
+  var path = d3.select(pathId),
   startPoint = pathStartPoint(path);
 
-  var marker = svg.append("circle");
+  var marker = animationGroup.append("circle");
   marker.attr("r", 7)
-    .attr("transform", "translate(" + startPoint + ")");
+      .attr("transform", "translate(" + startPoint + ")");
 
-  transition();
+  transition(marker, path);
+}
 
-  //Get path start point for placing marker
-  function pathStartPoint(path) {
-    console.log(path);
-    console.log(path.attr("d"));
-    var d = path.attr("d"),
-    dsplitted = d.split("c");
-    return dsplitted[1].split(",");
-  }
 
-  function transition() {
-    marker.transition()
-        .duration(7500)
-        .attrTween("transform", translateAlong(path.node()))
-        .each("end", transition);// infinite loop
-  }
+//Get path start point for placing marker
+function pathStartPoint(path) {
+  var d = path.attr("d"),
+  dsplitted = d.split("c");
+  return dsplitted[1].split(",")[0];
+}
 
-  function translateAlong(path) {
-    var l = path.getTotalLength();
-    return function(i) {
-      return function(t) {
-        var p = path.getPointAtLength(t * l);
-        return "translate(" + p.x + "," + p.y + ")";//Move marker
-      }
+function transition(marker, path) {
+  marker.transition()
+      .duration(30000)
+      .attrTween("transform", translateAlong(path.node()))
+      .on("end", function(){
+        transition(marker, path);
+      });// infinite loop
+}
+
+function translateAlong(path) {
+  var l = path.getTotalLength();
+  return function(i) {
+    return function(t) {
+      var p = path.getPointAtLength(t * l);
+      return "translate(" + p.x + "," + p.y + ")";//Move marker
     }
   }
+}
 
+function animationEnd(){
+  console.log("end");
 }
